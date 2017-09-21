1678dd937cc5da63cf5b6d045cc53d3cdca10a0d
Tom Drescher <tmdrescher@yahoo.com>
2017-09-21 17:13:42 -0400

    add selectors for controls

---
+++ scripts.js
@@ -0,0 +1,16 @@
+/*Get our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider');
+
+
+/*Build our functions */
+
+
+
+
+/*Hook up the event listeners */
