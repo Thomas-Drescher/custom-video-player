221ff845ae71bcf6db0432f678a720c5cd0a6063
Tom Drescher <tmdrescher@yahoo.com>
2017-09-21 18:32:37 -0400

    add event listener for skipButtons

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelector('.player__slider');
 
 
@@ -18,6 +18,9 @@ function updateButton() {
   const icon = this.paused ? '►' : '❚ ❚';
   toggle.textContent = icon;
 }
+function skip() {
+  console.log('skipping');
+}
 
 
 /*Hook up the event listeners */
@@ -25,3 +28,5 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
